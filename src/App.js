1f02727464a18575b4a8174b240d4d7722eb7899
Celeste Glavin <cmugla@gmail.com>
2017-06-05 16:19:02 -0400

    change color from white to black, yellow, orange, red

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    bgColor: {
+      backgroundColor: `rgb(255,255,255)`
+    }
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    this.setState({
+      bgColor: {
+        backgroundColor: `rgb(${window.scrollY > 255 * 2 && window.scrollY <= 255 * 3 ? 255 - (window.scrollY - 255 * 2) : 255},${window.scrollY > 255 && window.scrollY <= 255 * 2 ? 255 - (window.scrollY - 255) : window.scrollY <= 255 ? 255 : 0},${window.scrollY <= 255 ? 255 - window.scrollY : 0})`
+      }
+    })
+  }
+
   render() {
+    const { bgColor } = this.state
+
+    console.log( 'bgColor', bgColor )
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" style={bgColor}>
       </div>
     );
   }
